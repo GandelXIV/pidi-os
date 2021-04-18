17f94b21c372ccd90213835a04f3af22cf9aef1c
GandelXIV <filip@chovanec.com>
2021-04-18 13:26:31 +0200

    memcpy()

--- kernel/mem.h
+++ kernel/mem.h
@@ -1,3 +1,4 @@
+#include "../lib/type.h"
 #define MEMORY_INIT uint32_t** memory = (uint32_t**) KERNEL_MEMORY_OFFSET;
 
 void* kmalloc(uint size)
@@ -11,4 +12,13 @@ void kminit()
 {
   MEMORY_INIT
   *memory = (uint32_t*) KERNEL_MEMORY_OFFSET + 20;  // to make sure
+}
+
+void memcpy(bytes dest, bytes src, uint32_t size)
+{
+  for (uint32_t i = 0; i < size; ++i)
+  {
+    dest[i] = src[i];
+    ++i;
+  }
 }
