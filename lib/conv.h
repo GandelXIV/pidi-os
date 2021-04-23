97faab7981fe9bdb687f32c177ce42c60984246d
GandelXIV <filip@chovanec.com>
2021-04-23 15:12:07 +0200

    add char_to_hex() in conv.h

--- lib/conv.h
+++ lib/conv.h
@@ -84,3 +84,32 @@ char uint_to_char(uint number)
 {
   return uint32_to_char((uint32_t) number);
 }
+
+byte char_to_hex(char character)
+{
+  switch (character)
+  {
+    case '0': return 0x0;
+    case '1': return 0x1;
+    case '2': return 0x2;
+    case '3': return 0x3;
+    case '4': return 0x4;
+    case '5': return 0x5;
+    case '6': return 0x6;
+    case '7': return 0x7;
+    case '8': return 0x8;
+    case '9': return 0x9;
+    case 'a': return 0xA;
+    case 'A': return 0xA;
+    case 'b': return 0xB;
+    case 'B': return 0xB;
+    case 'c': return 0xC;
+    case 'C': return 0xC;
+    case 'd': return 0xD;
+    case 'D': return 0xD;
+    case 'e': return 0xE;
+    case 'E': return 0xE;
+    case 'f': return 0xF;
+    case 'F': return 0xF;
+  }
+}
