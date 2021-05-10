9740dcac1d93f8ed505a94e312f63e0f98eb8084
GandelXIV <filip@chovanec.com>
2021-05-10 12:03:04 +0200

    resize KERNEL_SIZE in boot/config.asm

--- boot/config.asm
+++ boot/config.asm
@@ -1,5 +1,5 @@
 BOOT_DRIVE db 0
-KERNEL_SIZE db 30 ; ammount of sectors in which the kernel is located
+KERNEL_SIZE db 33 ; ammount of sectors in which the kernel is located
 STACK_OFFSET db 9000
 KERNEL_OFFSET equ 0x1000
 MSG_REAL_MODE db "Started in 16-bit Real Mode", 0
