a47214f96b7d82e304014884b5b246251bb46952
moth-boi <dracek343@gmail.com>
2021-05-16 22:23:51 +0200

    more upgraded welcome message

--- kernel/messages.h
+++ kernel/messages.h
@@ -12,7 +12,7 @@
 #define KERNEL_INFO_INIT_START "Initialising...\n"            // start of init()
 #define KERNEL_INFO_INIT_DONE "Done!\n"                       // end of init()
 #define KERNEL_INFO_WELCOME "Welcome to Pidi-OS!\n"           // end of main() startup
-#define KERNEL_INFO_SHELL_WELCOME "$$$$$$$\\  $$\\       $$\\ $$\\  $$$$$$\\   $$$$$$\\  \n$$  __$$\\ \\__|      $$ |\\__|$$  __$$\\ $$  __$$\\ \n$$ |  $$ |$$\\  $$$$$$$ |$$\\ $$ /  $$ |$$ /  \\__|\n$$$$$$$  |$$ |$$  __$$ |$$ |$$ |  $$ |\\$$$$$$\\  \n$$  ____/ $$ |$$ /  $$ |$$ |$$ |  $$ | \\____$$\\ \n$$ |      $$ |$$ |  $$ |$$ |$$ |  $$ |$$\\   $$ |\n$$ |      $$ |\\$$$$$$$ |$$ | $$$$$$  |\\$$$$$$  |\n\\__|      \\__| \\_______|\\__| \\______/  \\______/ \nType 'help' to open ksh manual\n"        // on kshstart()
+#define KERNEL_INFO_SHELL_WELCOME "\n$$$$$$$\\  $$\\       $$\\ $$\\  $$$$$$\\   $$$$$$\\  \n$$  __$$\\ \\__|      $$ |\\__|$$  __$$\\ $$  __$$\\ \n$$ |  $$ |$$\\  $$$$$$$ |$$\\ $$ /  $$ |$$ /  \\__|\n$$$$$$$  |$$ |$$  __$$ |$$ |$$ |  $$ |\\$$$$$$\\  \n$$  ____/ $$ |$$ /  $$ |$$ |$$ |  $$ | \\____$$\\ \n$$ |      $$ |$$ |  $$ |$$ |$$ |  $$ |$$\\   $$ |\n$$ |      $$ |\\$$$$$$$ |$$ | $$$$$$  |\\$$$$$$  |\n\\__|      \\__| \\_______|\\__| \\______/  \\______/ \n\nType 'help' to open ksh manual\n"        // on kshstart()
 #define KERNEL_INFO_SHELL_UNKNOWN_COMMAND "Unknown command\n"               // ksh input unkown
 #define KERNEL_PANIC_MEMORY_INDEX_FULL "Kernel index is full!\nCAUSE: It seems that there have been too many memory pages allocated...\n" // when memory_index is full
 #define KERNEL_PANIC_MEMORY_FULL "Kernel memory is full!\nCAUSE: The system ran out of RAM..."
