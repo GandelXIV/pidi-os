1190dadfc3ca3a0456ca3367f6fd45d1343dad76
Gandel <filip@chovanec.com>
2021-05-17 17:58:50 +0200

    Updated config.h

--- kernel/config.h
+++ kernel/config.h
@@ -1,8 +1,8 @@
 // kernel.c
 #define DISPLAY_THEME GREEN_ON_BLACK
-#define KERNEL_STARTUP ksh_start();
+#define KERNEL_STARTUP ksh_start();   // on startup
 
 // mem.h
-#define KERNEL_MEMORY_OFFSET_START 0x10000
+#define KERNEL_MEMORY_OFFSET_START 0xffffff
 #define KERNEL_MEMORY_OFFSET_END	0xffffffff
-#define MEMORY_INDEX_BASE_SIZE 10000
+#define MEMORY_INDEX_BASE_SIZE 10000    // how many kmalloc calls you can do
