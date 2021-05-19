036662a22eea5cc14365bccfe0e3f149fec099af
Gandel <filip@chovanec.com>
2021-05-19 08:55:38 +0200

    move kernel.c to #pragma once

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,32 +1,12 @@
+#pragma once
 #include "config.h"
-
-#ifndef INCLUDED_TYPE
 #include "../lib/type.h"
-#endif
-
-#ifndef INCLUDED_IO
 #include "io.h"
-#endif
-
-#ifndef INCLUDED_ISR
 #include "../firmware/isr.h"
-#endif
-
-#ifndef INCLUDED_KMSG
 #include "kmsg.h"
-#endif
-
-#ifndef INCLUDED_MEM
 #include "mem.h"
-#endif
-
-#ifndef INCLUDED_KSH
 #include "ksh.h"
-#endif
-
-#ifndef INCLUDED_SMBIOS
 #include "../firmware/smbios.h"
-#endif
 
 static bool kernel_running;
 
