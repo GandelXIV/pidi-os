d7cf6237fe42164cb595df8a78abecbbf18241c6
Gandel <filip@chovanec.com>
2021-06-03 11:49:35 +0200

    potential fix for issue #19

--- kernel/advanced_cmds/write_to_file.h
+++ kernel/advanced_cmds/write_to_file.h
@@ -2,15 +2,21 @@ void ksh_write_to_file()
 {
   kprints("Enter file name: ");
   char* filename = (char*) kmalloc(256);
+  strcls(filename);
   kinputs(filename);
 
   char* data = (char*) kmalloc(256);
+  strcls(data);
   kprints("Enter data: ");
   kinputs(data);
   stradd(data, "\n");
 
   file_clean(filename);
-  file_writes(filename, data);
+  int response = file_writes(filename, data);
+  if (response == FILE_NOT_FOUND)
+  {
+    kprints("File not found!\n");
+  }
 
   kfree(filename);
   kfree(data);
