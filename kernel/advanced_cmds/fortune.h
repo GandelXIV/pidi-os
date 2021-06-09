8996e5f99b5bc826787fcd702f8aeaf25f66b0db
GandelXIV <61948151+GandelXIV@users.noreply.github.com>
2021-06-09 15:40:03 +0200

    Update fortune.h

--- kernel/advanced_cmds/fortune.h
+++ kernel/advanced_cmds/fortune.h
@@ -1,10 +1,11 @@
 #pragma once
+#include "../io.h"
 #include "../../drivers/display.h"
 
 void ksh_fortune() {
   const char fortunes[3] = {"Pohl's Law: Nothing is so good that somebody, somewhere, will not hate it.", "You either die a smart fella, or live long enough to become a fart smella", "Everyone asked you about your favorite dinosaur as a kid, now, nobody cares", "Even stock traders want to arrest hatred", "Generically, fiery liquors that produce madness in total abstainers.", "Our remedies oft in ourselves do lie,? Which we ascribe to heaven.", "One of the most conspicuous qualities of a man in security."};
-  kprintc("\n");
+  kprintc('\n');
   int RandIndex = rand() % sizeof(fortunes);
-  kprintc(arrayNum[RandIndex]);
-  kprintc("\n");
+  kprints(arrayNum[RandIndex]);
+  kprintc('\n');
 }
