35a02d93ac88f7d86cdce0be24133f15765dc858
PQCraft <58464017+PQCraft@users.noreply.github.com>
2021-06-09 19:51:17 -0400

    Modified keyboard driver

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -1,185 +1,230 @@
 #include "keyboard.h"
+#include <stddef.h>
 
-KeyEvent keyboard_old_event;
+//TODO: Fix interrupts pls so I can un-bruh this code
+
+bool keydown[384];
+
+static unsigned char keymap_lower[128] =
+"\e1234567890-=\b\tqwertyuiop[]\n\0asdfghjkl;'`\0\\zxcvbnm,./\0*\0 \0\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\0\0""789-456+1230.\0\0\0\x8A\x8B";
+
+static unsigned char keymap_upper[128] =
+"\e!@#$%^&*()_+\b\tQWERTYUIOP{}\n\0ASDFGHJKL:\"~\0|ZXCVBNM<>?\0*\0 \0\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\0\0""789-456+1230.\0\0\0\x8A\x8B";
+
+static unsigned char keymap_special[128] =
+"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0/\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x90\x8C\x92\0\x8D\0\x8E\0\x91\x8F\x93\x94\x7F\0\0\0\0\0\0\0\x95\x96";
+
+static unsigned char keymap_ascii[128] =
+"\e1234567890-=\b\t\x11\x17\x05\x12\x14\x19\x15\x09\x0F\x10[]\r\0\x01\x13\x04\x06\x07\x08\x0A\x0B\x0C;'`\0\\\x1A\x18\x03\x16\x02\x0E\x0D,./\0*\0 \0\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8A\0""789-456+1230.\0\0\0\x8A\x8B";
+
+bool c_lock = false, n_lock = false, s_lock = false;
+bool lsft_d = false, lctl_d = false, lalt_d = false;
+bool rsft_d = false, rctl_d = false, ralt_d = false;
+
+bool kb_alt_char = false;
+
+unsigned int kbin;
+static bool key = false;
+
+static unsigned char c = 0;
+
+bool xE0 = false;
+
+int tmpptr = 0;
+unsigned char tmpbuf[4] = "\0\0\0";
+
+void kbh();
+
+static unsigned char ch = 0;
+
+char kinputc() {
+    do {
+        if (kbin >= 128 || kbin == 0xE0) ch = 0;
+        kbh();
+    } while (c == ch || kbin >= 128);
+    ch = c;
+    return (char)ch;
+}
+
 byte scan_code_set;
 
-KeyEvent scan_code_to_key_event(byte code)
-{
-  if (scan_code_set == 0x1)
-  {
-    switch (code) {
-      case 0x2: return KEY_DOWN_1;
-      case 0x3: return KEY_DOWN_2;
-      case 0x4: return KEY_DOWN_3;
-      case 0x5: return KEY_DOWN_4;
-      case 0x6: return KEY_DOWN_5;
-      case 0x7: return KEY_DOWN_6;
-      case 0x8: return KEY_DOWN_7;
-      case 0x9: return KEY_DOWN_8;
-      case 0xA: return KEY_DOWN_9;
-      case 0xB: return KEY_DOWN_0;
-      case 0xC: return KEY_DOWN_DASH;
-      case 0xD: return KEY_DOWN_EQUALS;
-      case 0xE: return KEY_DOWN_BACKSPACE;
-      case 0xF: return KEY_DOWN_TAB;
-      case 0x10: return KEY_DOWN_Q;
-      case 0x11: return KEY_DOWN_W;
-      case 0x12: return KEY_DOWN_E;
-      case 0x13: return KEY_DOWN_R;
-      case 0x14: return KEY_DOWN_T;
-      case 0x15: return KEY_DOWN_Y;
-      case 0x16: return KEY_DOWN_U;
-      case 0x17: return KEY_DOWN_I;
-      case 0x18: return KEY_DOWN_O;
-      case 0x19: return KEY_DOWN_P;
-      case 0x1A: return KEY_DOWN_LEFT_SQUARE_BRACKET;
-      case 0x1B: return KEY_DOWN_RIGHT_SQUARE_BRACKET;
-      case 0x1C: return KEY_DOWN_ENTER;
-      case 0x1E: return KEY_DOWN_A;
-      case 0x1F: return KEY_DOWN_S;
-      case 0x20: return KEY_DOWN_D;
-      case 0x21: return KEY_DOWN_F;
-      case 0x22: return KEY_DOWN_G;
-      case 0x23: return KEY_DOWN_H;
-      case 0x24: return KEY_DOWN_J;
-      case 0x25: return KEY_DOWN_K;
-      case 0x26: return KEY_DOWN_L;
-      case 0x27: return KEY_DOWN_SEMICOLON;
-      case 0x28: return KEY_DOWN_BACKSLASH;
-      case 0x29: return KEY_DOWN_BACKTICK;
-      case 0x2A: return KEY_DOWN_LEFT_SHIFT;
-      case 0x2B: return KEY_DOWN_BACKSLASH;
-      case 0x2C: return KEY_DOWN_Z;
-      case 0x2D: return KEY_DOWN_X;
-      case 0x2E: return KEY_DOWN_C;
-      case 0x2F: return KEY_DOWN_V;
-      case 0x30: return KEY_DOWN_B;
-      case 0x31: return KEY_DOWN_N;
-      case 0x32: return KEY_DOWN_M;
-      case 0x33: return KEY_DOWN_COMMA;
-      case 0x34: return KEY_DOWN_DOT;
-      case 0x35: return KEY_DOWN_FRONT_SLASH;
-      case 0x36: return KEY_DOWN_RIGHT_SHIFT;
-      case 0x39: return KEY_DOWN_SPACE;
-      case 0x53: return KEY_DOWN_DOT;
-      /*
-      case 0x37: return KEY_DOWN_'*'; // keypad
-      case 0x47: return KEY_DOWN_'7'; // keypad
-      case 0x48: return KEY_DOWN_'8'; // keypad
-      case 0x49: return KEY_DOWN_'9'; // keypad
-      case 0x4A: return KEY_DOWN_'-'; // keypad
-      case 0x4B: return KEY_DOWN_'4'; // keypad
-      case 0x4C: return KEY_DOWN_'5'; // keypad
-      case 0x4D: return KEY_DOWN_'6'; // keypad
-      case 0x4E: return KEY_DOWN_'+'; // keypad
-      case 0x4F: return KEY_DOWN_'1'; // keypad
-      case 0x50: return KEY_DOWN_'2'; // keypad
-      case 0x51: return KEY_DOWN_'3'; // keypad
-      case 0x52: return '0'; // keypad
-      */
-      default: return KEY_VOID;  // no printable character
+//#include <util.c>
+int atoi(char* str) {
+    int res = 0;
+    for (int i = 0; str[i] != '\0'; ++i) {
+        res = res * 10 + str[i] - '0';
     }
-  }
+    return res;
 }
 
-byte read_keyboard_stream()
-{
-  return port_byte_in(REG_KEYBOARD_DATA);
+void kbport_byte_out(unsigned char byte) {
+    unsigned char tmp;
+    int tries = 0;
+    do {
+        port_byte_out(0x60, byte);
+        do {
+            tmp = port_byte_in(0x60);
+            //kprintf("0x%_x (0x%_x): 0x%_x\n", port, 4, byte, 2, tmp, 2);
+        } while (tmp == 0);
+        tries++;
+    } while (tmp == 0xFE && tries < 3);
+}    
+
+void kbupdateleds() {
+    kbport_byte_out(0xED);
+    kbport_byte_out((s_lock & 1) + ((n_lock & 1) << 1) + ((c_lock & 1) << 2));
 }
 
-KeyEvent keyboard_get()
-{
-  KeyEvent event = scan_code_to_key_event(read_keyboard_stream());
-  if (event == keyboard_old_event) return KEY_VOID;
-  // temporary fix
-  if (event == KEY_DOWN_LEFT_SHIFT || event == KEY_DOWN_RIGHT_SHIFT) return KEY_VOID;
-  keyboard_old_event = event;
-  return event;
+void kbh() {
+    kbin = port_byte_in(0x60);
+    //kprintf("Keyboard: 0x%_x [%d][%d|%d|%d][%d|%d|%d][%d|%d|%d] ", kbin, 2, xE0, c_lock, n_lock, s_lock, lsft_d, lctl_d, lalt_d, rsft_d, rctl_d, ralt_d);
+    kb_alt_char = false;
+    c = 0;
+    if (kbin == 0xE0) {
+        xE0 = true;
+        goto kbend;
+    }
+    if (kbin < 128) {
+        if (!n_lock && kbin >= 0x47 && kbin <= 0x52) xE0 = true;
+        if (xE0) {
+            switch (kbin) {
+                case 0x1D:
+                    rctl_d = true;
+                    break;
+                case 0x38:
+                    ralt_d = true;
+                    break;
+                default:
+                    c = keymap_special[kbin - 1];
+                    keydown[kbin + 127] = true;
+                    break;
+            }
+            xE0 = false;
+        } else {
+            switch (kbin) {
+                case 0x1D:
+                    lctl_d = true;
+                    break;
+                case 0x2A:
+                    lsft_d = true;
+                    break;
+                case 0x36:
+                    rsft_d = true;
+                    break;
+                case 0x38:
+                    lalt_d = true;
+                    break;
+                case 0x3A:
+                    c_lock = !c_lock;
+                    kbupdateleds();
+                    break;
+                case 0x45:
+                    n_lock = !n_lock;
+                    kbupdateleds();
+                    break;
+                case 0x46:
+                    s_lock = !s_lock;
+                    kbupdateleds();
+                    break;
+                default:
+                    if (lalt_d || ralt_d) {
+                        if (keymap_lower[kbin - 1] >= '0' && keymap_lower[kbin - 1] <= '9' && tmpptr < 3 && kbin >= 0x47 && kbin <= 0x52) {
+                            tmpbuf[tmpptr] = keymap_lower[kbin - 1];
+                            tmpptr++;
+                        }
+                    } else {
+                        if (lctl_d || rctl_d) {
+                            c = keymap_ascii[kbin - 1];
+                        } else {
+                            if (c_lock) {
+                                if (lsft_d || rsft_d) {
+                                    c = keymap_lower[kbin - 1];
+                                } else {
+                                    c = keymap_upper[kbin - 1];
+                                }
+                            } else {
+                                if (lsft_d || rsft_d) {
+                                    c = keymap_upper[kbin - 1];
+                                } else {
+                                    c = keymap_lower[kbin - 1];
+                                }
+                            }
+                        }
+                        keydown[kbin - 1] = true;
+                    }
+                    break;
+            }
+        }
+    } else {
+        kbin -= 128;
+        if (!n_lock && kbin >= 0x47 && kbin <= 0x52) xE0 = true;
+        if (xE0) {
+            switch (kbin) {
+                case 0x1D:
+                    rctl_d = false;
+                    break;
+                case 0x38:
+                    ralt_d = false;
+                    break;
+                default:
+                    keydown[kbin + 127] = false;
+                    break;
+            }
+            xE0 = false;
+        } else {
+            switch (kbin) {
+                case 0x1D:
+                    lctl_d = false;
+                    break;
+                case 0x2A:
+                    lsft_d = false;
+                    break;
+                case 0x36:
+                    rsft_d = false;
+                    break;
+                case 0x38:
+                    lalt_d = false;
+                    break;
+                case 0x3A:
+                    break;
+                case 0x45:
+                    break;
+                case 0x46:
+                    break;
+                default:
+                    keydown[kbin - 1] = false;
+                    break;
+            }
+            kbin += 128;
+        }
+        if (!(lalt_d || ralt_d) && tmpptr > 0) {
+            //kprintf("[%d] {", tmpptr);
+            c = atoi(tmpbuf) % 256;
+            //kprintf("%s} (%d) [%d] ", tmpbuf, c, tmpptr);
+            for (tmpptr = 0; tmpptr < 3; tmpptr++) {
+                tmpbuf[tmpptr] = 0;
+            }
+            tmpptr = 0;
+            kb_alt_char = true;
+        }
+    }
+    kbend:
+    //kputchar(c);
+    //kputchar('\n');
+    //if ((lctl_d || rctl_d) && (lalt_d || ralt_d) && c == 127) reboot();
+    //if (xE0 && kbin == 0x5E) reboot();
+    key = true;
 }
 
 void on_key_event(registers_t registers)
 {
-  kprints("Registered event!");
+    //kprints("Registered event!");
 }
 
-void keyboard_init()
-{
-  scan_code_set = 0x1;
-  register_interrupt_handler(IRQ1, on_key_event);
-}
-
-char key_event_to_char(KeyEvent code)
-{
-  switch (code) {
-    case KEY_DOWN_1: return '1';
-    case KEY_DOWN_2: return '2';
-    case KEY_DOWN_3: return '3';
-    case KEY_DOWN_4: return '4';
-    case KEY_DOWN_5: return '5';
-    case KEY_DOWN_6: return '6';
-    case KEY_DOWN_7: return '7';
-    case KEY_DOWN_8: return '8';
-    case KEY_DOWN_9: return '9';
-    case KEY_DOWN_0: return '0';
-    case KEY_DOWN_DASH: return '-';
-    case KEY_DOWN_EQUALS: return '=';
-    case KEY_DOWN_BACKSPACE: return '\b';
-    case KEY_DOWN_TAB: return '\t';
-    case KEY_DOWN_Q: return 'q';
-    case KEY_DOWN_W: return 'w';
-    case KEY_DOWN_E: return 'e';
-    case KEY_DOWN_R: return 'r';
-    case KEY_DOWN_T: return 't';
-    case KEY_DOWN_Y: return 'y';
-    case KEY_DOWN_U: return 'u';
-    case KEY_DOWN_I: return 'i';
-    case KEY_DOWN_O: return 'o';
-    case KEY_DOWN_P: return 'p';
-    case KEY_DOWN_LEFT_SQUARE_BRACKET: return '[';
-    case KEY_DOWN_RIGHT_SQUARE_BRACKET: return ']';
-    case KEY_DOWN_ENTER: return '\n';
-    case KEY_DOWN_A: return 'a';
-    case KEY_DOWN_S: return 's';
-    case KEY_DOWN_D: return 'd';
-    case KEY_DOWN_F: return 'f';
-    case KEY_DOWN_G: return 'g';
-    case KEY_DOWN_H: return 'h';
-    case KEY_DOWN_J: return 'j';
-    case KEY_DOWN_K: return 'k';
-    case KEY_DOWN_L: return 'l';
-    case KEY_DOWN_SEMICOLON: return ';';
-    case KEY_DOWN_APOSTROPHE: return '\'';
-    case KEY_DOWN_BACKTICK: return '`';
-    case KEY_DOWN_BACKSLASH: return '\\';
-    case KEY_DOWN_Z: return 'z';
-    case KEY_DOWN_X: return 'x';
-    case KEY_DOWN_C: return 'c';
-    case KEY_DOWN_V: return 'v';
-    case KEY_DOWN_B: return 'b';
-    case KEY_DOWN_N: return 'n';
-    case KEY_DOWN_M: return 'm';
-    case KEY_DOWN_COMMA: return ',';
-    case KEY_DOWN_DOT: return '.';
-    case KEY_DOWN_FRONT_SLASH: return '/';
-    case KEY_DOWN_SPACE: return ' ';
-    default: return UNPRINTALBE_CHAR;  // no printable character
-  }
-}
-
-char kinputc()
-{
-  KeyEvent event = KEY_VOID;
-  char output = UNPRINTALBE_CHAR;
-
-  while (output == UNPRINTALBE_CHAR)
-  {
-    // wait for new keyboard event
-    while (event == KEY_VOID)
-    {
-      event = keyboard_get();
-    }
-    // translate event to char
-    output = key_event_to_char(event);
-  }
-  return output;
+void keyboard_init() {
+    key = false;
+    for (int i = 0; i < 384; i++) {keydown[i] = false;}
+    n_lock = true;
+    kbupdateleds();
+    register_interrupt_handler(IRQ1, kbh);
 }
